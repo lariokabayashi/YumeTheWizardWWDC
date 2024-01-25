4751c3810133cc595daf043868e45939cd42e538
lariokabayashi <l247220@dac.unicamp.br>
2024-01-25 14:29:22 -0300

    changes with model

---
+++ KikiTheWizard.swiftpm/ImageClassifier.swift
@@ -0,0 +1,8 @@
+//
+//  ImageClassifier.swift
+//  KikiTheWizard
+//
+//  Created by Larissa Okabayashi on 25/01/24.
+//
+
+import Foundation
---
+++ KikiTheWizard.swiftpm/InventoryBiasScene.swift
@@ -0,0 +1,8 @@
+//
+//  InventoryBiasScene.swift
+//  KikiTheWizard
+//
+//  Created by Larissa Okabayashi on 24/01/24.
+//
+
+import Foundation
